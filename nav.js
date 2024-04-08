001b0588c2ad022d9d8238519652e249dc6ec3ca
nityam10000 <114255270+nityam10000@users.noreply.github.com>
2024-04-09 03:17:43 +0530

    Just made new website

---
+++ nav.js
@@ -0,0 +1,23 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+navToggle.addEventListener('click',()=>{
+    const visibility = primaryNav.getAttribute('data-visible');
+    console.log(visibility);
+    if(visibility === "false"){
+        primaryNav.setAttribute("data-visible",true);
+        navToggle.setAttribute("aria-expanded",true);
+    }else{
+        primaryNav.setAttribute("data-visible",false);
+        navToggle.setAttribute("aria-expanded",false);
+    }
+});
+
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 4000);
+});
