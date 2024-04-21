996e49e63b408e2329f7bd84b1833b7371674be3
nityam10000 <114255270+nityam10000@users.noreply.github.com>
2024-04-21 23:21:44 +0530

    added login and sign up pages with backend services

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+const path = require('path');
+const collection=require('./mongodb');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '')));
+
+// Define routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'login.html'));
 });
-app.get("/furniture", function (req, res) {
-    res.sendFile(__dirname + "/views/furniture.html");
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname,'signup.html'));
 });
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001');
+
+// Your other routes...
+app.post("/signup",async(req,res)=>{
+    const data={
+        firstName:req.body.firstName,
+        secondName:req.body.secondName,
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        address1:req.body.address1,
+        address2:req.body.address2,
+        city:req.body.city,
+        state:req.body.state,
+        zip:req.body.zip
+    }
+await collection.insertMany([data])
+res.sendFile(path.join(__dirname, '/'));
+})
+
+app.post("/login",async(req,res)=>{
+    try {
+        const check=await collection.findOne({username:req.body.username})
+        if(check.password===req.body.password){
+            res.sendFile(path.join(__dirname, 'index.html'));
+
+        }
+        else{
+            res.send("Wrong password")
+        }
+    } catch{
+        res.send("Wrong details")
+    }
+    
+})
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
 });
-
-app.use(express.static('public'));
-
-
-
-
-
-
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://nityamsalgotra900:qWcVQHstuvaKYl86@cluster0.1sx31vg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
-}
-run().catch(console.dir);
---
+++ mongodb.js
@@ -0,0 +1,53 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://localhost:27017/LoginSignUpPage2")
+.then(()=>{
+    console.log("mongo connected");
+})
+.catch(()=>{
+    console.log("failed to connect");
+})
+const LogInSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        require:true
+    },
+    SecondName:{
+        type:String,
+        require:true
+    },
+    username:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    address1:{
+        type:String,
+        require:true
+    },
+    address2:{
+        type:String,
+        require:true
+    },
+    city:{
+        type:String,
+        require:true
+    },
+    state:{
+        type:String,
+        require:true
+    },
+    zip:{
+        type:String,
+        require:true
+    },
+})
+const collection=new mongoose.model("Collection2",LogInSchema);
+
+module.exports=collection
